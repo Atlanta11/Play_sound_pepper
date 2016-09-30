1306779f6d1d2427fa5483d114a7a8249c357f4b
Enjoy <Enjoy>
2016-09-30 16:06:10 +0200

    Initial commit

---
+++ app/src/main/java/enterprise/sample/com/play_sound_pepper/MainActivity.java
@@ -0,0 +1,45 @@
+package enterprise.sample.com.play_sound_pepper;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aldebaran.qi.sdk.object.actuation.Animate;
+import com.aldebaran.qi.sdk.object.actuation.Animation;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mediaPlayer = MediaPlayer.create(this, R.raw.elephant_sound);
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = MediaPlayer.create(this, R.raw.elephant_sound);
+        }
+        Animation animation = Animation.fromResources(this, R.raw.elephant);
+        Animate animate = new Animate(this);
+        animate.setOnStartedListener(new Animate.OnStartedListener() {
+            @Override
+            public void onStarted() {
+                mediaPlayer.start();
+            }
+        });
+        animate.run(animation);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mediaPlayer.stop();
+    }
+}
